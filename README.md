d5c18d90d41ab7f1736764fd370957e1ac9bdbb8
bur. <bur.and.by@gmail.com>
2026-08-07 11:56:59 +0300

    Add MP3 and FLAC format support

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # YTDLP-KHInsider
-A plugin that allows you to download MP3s from KHInsider via yt-dlp
+A plugin that allows you to download MP3 and FLAC tracks from KHInsider via yt-dlp.
+
+The extractor recognizes audio sources in `<audio>`/`<source>` elements as well as
+direct MP3/FLAC download links. The output format is inferred from the source URL
+or MIME type, so yt-dlp can apply the correct filename extension automatically.
 
 ## Installing
 Run: <!-- wow, I really need a good readme --> <br>
--- yt_dlp_plugins/extractor/khinsider.py
+++ yt_dlp_plugins/extractor/khinsider.py
@@ -1,121 +1,103 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from yt_dlp.extractor.common import InfoExtractor
-from yt_dlp.utils import clean_html, ExtractorError
+import html
 import re
+from urllib.parse import urljoin, urlparse
 
-class KHInsiderTrackIE(InfoExtractor):
-    __version__ = "0.1.0"
+from yt_dlp.extractor.common import InfoExtractor
+from yt_dlp.utils import ExtractorError, clean_html, determine_ext, unescapeHTML
+
+
+class _KHInsiderBaseIE(InfoExtractor):
+    _BASE_URL = 'https://downloads.khinsider.com'
+
+    def _media_formats(self, webpage, page_url):
+        """Find all usable audio sources, regardless of markup or format."""
+        candidates = []
+        # Audio and source tags may put src before or after type, and use either quote style.
+        for tag in re.findall(r'<(?:audio|source)\b[^>]*>', webpage, flags=re.I):
+            attrs = dict((key.lower(), html.unescape(value)) for key, value in re.findall(
+                r'''([\w:-]+)\s*=\s*["']([^"']+)["']''', tag, flags=re.I))
+            if attrs.get('src'):
+                candidates.append((attrs['src'], attrs.get('type')))
+        # Some pages expose a direct download link without an audio element.
+        for value in re.findall(r'''<a\b[^>]+href\s*=\s*["']([^"']+\.(?:mp3|flac)(?:\?[^"']*)?)["']''', webpage, flags=re.I):
+            candidates.append((html.unescape(value), None))
+        formats, seen = [], set()
+        for source, mime in candidates:
+            source = urljoin(page_url, unescapeHTML(source))
+            ext = determine_ext(source) or ((mime or '').split('/')[-1].lower() if mime else None)
+            if ext in ('mp3', 'flac') and source not in seen:
+                seen.add(source)
+                formats.append({
+                    'url': source,
+                    'ext': ext,
+                    'format_id': ext,
+                    'vcodec': 'none',
+                    'quality': 1 if ext == 'flac' else 0,
+                })
+        return formats
+
+    def _title(self, webpage, fallback):
+        title = self._html_search_regex(
+            (r'<meta\s+[^>]*property=["\']og:title["\'][^>]*content=["\']([^"\']+)',
+             r'<h2\b[^>]*>(.*?)</h2>', r'<title\b[^>]*>(.*?)</title>'),
+            webpage, 'title', default=None, fatal=False)
+        return clean_html(title).strip() if title else fallback
+
+
+class KHInsiderTrackIE(_KHInsiderBaseIE):
+    __version__ = '0.2.0'
     _WORKING = True
-    IE_NAME = "khinsider_track"
-    IE_DESC = "Bur's KHInsider VGM Downloader [Downloads tracks in mp3]"
-    IE_BUG_REPORT = "Please report this issue on https://github.com/burandby/bursytdlps"
-    
-    _VALID_URL = r'https?://downloads\.khinsider\.com/game-soundtracks/album/[^/]+/(?P<id>[\w.%]+)'
-    _TESTS = [
-        {
-            'url': 'https://downloads.khinsider.com/game-soundtracks/album/acquaria-windows-gamerip-2003/01.%2520Acquaria-Theme.mp3',
-            'info_dict': {
-                'id': '01.%2520Acquaria-Theme.mp3',
-                'title': 'Acquaria-Theme',
-                'ext': 'mp3',
-            }
-        }
-    ]
-    
+    IE_NAME = 'khinsider_track'
+    IE_DESC = "Bur's KHInsider VGM Downloader [MP3 and FLAC]"
+    IE_BUG_REPORT = 'Please report this issue on https://github.com/burandby/bursytdlps'
+    _VALID_URL = r'https?://downloads\.khinsider\.com/game-soundtracks/album/[^/]+/(?P<id>[^/?#]+)'
+
     def _real_extract(self, url):
-        webpage = self._download_webpage(url, None)
-        
-        # Extract song title
-        title = self._html_search_regex(r'Song name:\s*(.*)', webpage, 'song title', default=None)
-        id = url.split('/')[-1]
-        
-        audio = self._html_search_regex(r'<audio.*?src="([^"]+)"', webpage, 'MP3 URL', default=None)
-        if not audio:
-            raise ExtractorError('Could not find MP3 URL')
-        
-        return {
-            'id': id,
-            'title': title,
-            'url': audio,
-            'ext': 'mp3',
-        }
-        
+        track_id = self._match_id(url)
+        webpage = self._download_webpage(url, track_id)
+        title = self._html_search_regex(r'Song\s+name\s*:\s*</?[^>]*>\s*([^<\r\n]+)', webpage,
+                                        'song title', default=None, fatal=False)
+        title = clean_html(title).strip() if title else clean_html(track_id.rsplit('.', 1)[0]).strip()
+        formats = self._media_formats(webpage, url)
+        if not formats:
+            raise ExtractorError('Could not find an MP3 or FLAC URL')
+        return {'id': track_id, 'title': title, 'formats': formats}
 
-class KhinsiderAlbumIE(InfoExtractor):
-    __version__ = "0.1.0"
+
+class KhinsiderAlbumIE(_KHInsiderBaseIE):
+    __version__ = '0.2.0'
     _WORKING = True
-    IE_NAME = "khinsider:album"
-    IE_DESC = "Bur's KHInsider VGM Downloader [Downloads albums in MP3]"
-    IE_BUG_REPORT = "Please report this issue on https://github.com/burandby/bursytdlps"
-    
+    IE_NAME = 'khinsider:album'
+    IE_DESC = "Bur's KHInsider VGM Downloader [MP3 and FLAC]"
+    IE_BUG_REPORT = 'Please report this issue on https://github.com/burandby/bursytdlps'
     _VALID_URL = r'https?://downloads\.khinsider\.com/game-soundtracks/album/(?P<id>[\w-]+)'
-    _TESTS = [
-        {
-            'url': 'https://downloads.khinsider.com/game-soundtracks/album/drawful-2-original-unofficial-soundtrack-family-computer-ios-linux-macos-ps4-gamerip-2016',
-            'info_dict': {
-                'id': 'drawful-2-original-unofficial-soundtrack-family-computer-ios-linux-macos-ps4-gamerip-2016',
-                'title': 'Drawful 2 Original Unofficial Soundtrack',
-            },
-            'playlist_mincount': 13,
-        },
-        {
-            'url': 'https://downloads.khinsider.com/game-soundtracks/album/minecraft',
-            'info_dict': {
-                'id': 'minecraft',
-                'title': 'Minecraft Soundtrack - Volume Alpha and Beta (Complete Edition)',
-            },
-            'playlist_mincount': 54,
-        },
-        {
-            'url': 'https://downloads.khinsider.com/game-soundtracks/album/persona-5-royal-the-complete-soundtrack',
-            'info_dict': {
-                'id': 'persona-5-royal-the-complete-soundtrack',
-                'title': 'Persona 5 Royal - The Complete Soundtrack',
-            },
-            'playlist_mincount': 140,
-        }
-    ]
 
     def _real_extract(self, url):
         album_id = self._match_id(url)
         webpage = self._download_webpage(url, album_id)
-        
-        # Extract album title
-        title = self._html_search_regex(r'<h2>(.+?)</h2>', webpage, 'album title', default=None)
-        if not title:
-            title = album_id.replace('-', ' ').title()
-        
-        entries = []
-        for song_path in re.findall(r'<a href="(/game-soundtracks/album/[^"]+)">(.+?)</a>', webpage):
-            song_url = f'https://downloads.khinsider.com{song_path[0]}'
-            song_title = clean_html(song_path[1])
-            
-            if song_title == 'get_app':
-                # Stupid workaround for the get_app icon which also leads to the same download page
-                # Probably would be a good idea to just remove duplicate links but that would require
-                # to add an array that would store all the links but that would just be a waste of RAM
-                # TODO: Find a better way to do this.
+        title = self._title(webpage, album_id.replace('-', ' ').title())
+        songlist = self._search_regex(
+            r'<table\b[^>]*\bid=["\']songlist["\'][^>]*>(.*?)</table>',
+            webpage, 'song list', flags=re.I | re.S)
+        entries, seen = [], set()
+        for path, raw_title in re.findall(r'<a\b[^>]*href=["\'](/game-soundtracks/album/[^"\']+)["\'][^>]*>(.*?)</a>', songlist, re.I | re.S):
+            song_url = urljoin(self._BASE_URL, path)
+            song_id = urlparse(song_url).path.rsplit('/', 1)[-1]
+            # Album navigation contains a self-link (currently labelled "Change Log").
+            if song_id == album_id or song_id in seen or song_id.lower() == 'get_app':
                 continue
-            
+            seen.add(song_id)
+            song_title = clean_html(raw_title).strip()
             try:
                 song_page = self._download_webpage(song_url, album_id, note=f'Downloading page for {song_title}')
-                mp3_url = self._html_search_regex(r'<audio.*?src="([^"]+)"', song_page, 'MP3 URL', default=None)
-                sid = url.split('/')[-1]
-
-                if not mp3_url:
-                    self.report_warning(f'Could not find MP3 URL for {song_title}')
+                formats = self._media_formats(song_page, song_url)
+                if not formats:
+                    self.report_warning(f'Could not find MP3 or FLAC URL for {song_title}')
                     continue
-
-                entries.append({
-                    'id': sid,
-                    'title': song_title,
-                    'url': mp3_url,
-                    'ext': 'mp3',
-                })
-            except ExtractorError as e:
-                self.report_warning(f'Error extracting {song_title}: {e}')
-                continue
-
+                entries.append({'id': song_id, 'title': song_title or song_id, 'formats': formats})
+            except ExtractorError as err:
+                self.report_warning(f'Error extracting {song_title}: {err}')
         return self.playlist_result(entries, album_id, title)
